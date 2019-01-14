40753fcee08eda47bf5aa06d5bb7036274e9ad78
clair13 <theclairvoyant1313@yahoo.gr>
2019-01-14 21:44:15 +0200

    Implement actions for Citations controller and  corresponding views

--- app/controllers/citations_controller.rb
+++ app/controllers/citations_controller.rb
@@ -1,10 +1,52 @@
 class CitationsController < ApplicationController
+  before_action :set_citation, only: [:show, :update, :edit, :destroy]
+
+
   def index
     @citations = Citation.all
   end
 
-  def new
+  def show
   end
-  
 
+
+  def new
+    @citation = Citation.new
+  end
+
+  def edit
+  end
+
+  def create
+    @citation = Citation.new(citation_params)
+
+    if @citation.save
+      redirect_to action: "index"
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    if @citation.update(citation_params)
+      redirect_to @citation
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @citation.destroy
+    redirect_to citations_path
+  end
+
+  private
+
+  def citation_params
+    params.require(:citation).permit(:description, :name)
+  end
+
+  def set_citation
+    @citation = Citation.find(params[:id])
+  end
 end
